3165fbabd5a0a27ba2ed2446312d9b56213987d8
Rasel <rrasel141@gmail.com>
2020-11-30 21:40:08 +0600

    setup redux and fetch products data by redux

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,27 @@
+import {
+  PRODUCT_LIST_FAILURE,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+} from "../constants/productConstant";
+
+export const productListReducer = (
+  state = { loading: true, products: [] },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true };
+    case PRODUCT_LIST_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload,
+      };
+    case PRODUCT_LIST_FAILURE:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
